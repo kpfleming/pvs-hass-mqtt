03faa0c7bb9020c608da855a25c1e4347dcdfbb0
Kevin P. Fleming <kevin@km6g.us>
2022-07-29 07:37:25 -0400

    add 'systemd' optional feature

--- workflow-support/make_hatch_image.sh
+++ workflow-support/make_hatch_image.sh
@@ -6,7 +6,7 @@ scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 lintdeps=(shellcheck)
 pydeps=(build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev)
 pyversions=(3.8.13 3.9.13 3.10.5 3.11.0b5)
-hatchenvs=(lint lint-action ci)
+hatchenvs=(lint lint-action ci ci-systemd)
 
 c=$(buildah from debian:bullseye)
 
