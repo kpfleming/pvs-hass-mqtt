658f2ed134cf3c945b056bdeca0cecc785473dd6
Kevin P. Fleming <kevin@km6g.us>
2022-08-05 16:44:55 -0400

    Move resettable_logging fixture to a higher-level package so it can be reused

--- tests/pvs_hass_mqtt/logging/conftest.py
+++ tests/pvs_hass_mqtt/logging/conftest.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
 
-import importlib
-import logging
-from collections.abc import Iterator
 from types import ModuleType
 
 import pytest
 
 
 @pytest.fixture(autouse=True)
-def resettable_logging() -> Iterator[ModuleType]:
-    yield logging
-    logging.shutdown()
-    importlib.reload(logging)
+def auto_resettable_logging(resettable_logging: ModuleType) -> ModuleType:
+    return resettable_logging
