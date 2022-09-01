e190799eaa739bff9522c2892b3799e73498b935
Kevin P. Fleming <kevin@km6g.us>
2022-09-01 17:48:34 -0400

    Add connect/disconnect MQTT callbacks, and tests

--- src/hass_mqtt/mqtt.py
+++ src/hass_mqtt/mqtt.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
 import logging
+from collections.abc import Mapping
 
 import paho.mqtt.client as mqtt_client  # type: ignore
 from attrs import define, field
 
 logger = logging.getLogger(__name__)
+
+
+def _on_connect(
+    client: mqtt_client.Client, userdata: MQTT, flags: Mapping[str, int], rc: int
+) -> None:
+    userdata._on_connect(flags, rc)
+
+
+def _on_disconnect(client: mqtt_client.Client, userdata: MQTT, rc: int) -> None:
+    userdata._on_disconnect(rc)
 
 
 @define(kw_only=True)
@@ -23,7 +34,15 @@ class MQTT:
     def connect(self) -> None:
         client = mqtt_client.Client(client_id=self.client_id, userdata=self)
         client.enable_logger()
+        client.on_connect = _on_connect
+        client.on_disconnect = _on_disconnect
         if self.username:
             client.username_pw_set(self.username, self.password)
         client.connect(host=self.broker, port=self.port, keepalive=self.keep_alive)
         self._client = client
+
+    def _on_connect(self, flags: Mapping[str, int], rc: int) -> None:
+        pass
+
+    def _on_disconnect(self, rc: int) -> None:
+        pass
---
+++ tests/hass_mqtt/client/test_callbacks.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from pytest_mock import MockerFixture
+
+import hass_mqtt.mqtt
+from hass_mqtt import MQTT
+
+
+def test_on_connect(mocker: MockerFixture) -> None:
+    """Ensure that the on_connect callback forwards to the MQTT object,
+    and passes along the 'flags' and 'rc' arguments."""
+    mock_func = mocker.patch.object(MQTT, "_on_connect", autospec=True)
+    mqtt = MQTT(broker="", port=0, keep_alive=0, qos=0, hass_topic_prefix="")
+    hass_mqtt.mqtt._on_connect(None, mqtt, {"flag": 42}, -42)
+    mock_func.assert_called_once_with(mqtt, {"flag": 42}, -42)
+
+
+def test_on_discconnect(mocker: MockerFixture) -> None:
+    """Ensure that the on_disconnect callback forwards to the MQTT object,
+    and passes along the 'rc' argument."""
+    mock_func = mocker.patch.object(MQTT, "_on_disconnect", autospec=True)
+    mqtt = MQTT(broker="", port=0, keep_alive=0, qos=0, hass_topic_prefix="")
+    hass_mqtt.mqtt._on_disconnect(None, mqtt, -42)
+    mock_func.assert_called_once_with(mqtt, -42)
